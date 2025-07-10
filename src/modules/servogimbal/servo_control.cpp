a3b5066b9de3e9fa3a3a2e6f0771305442e21457
ta-jetson <150237120+ta-jetson@users.noreply.github.com>
2025-07-10 12:40:30 +0530

    Update servo_control.cpp

--- src/modules/servogimbal/servo_control.cpp
+++ src/modules/servogimbal/servo_control.cpp
@@ -1,21 +1,4 @@
-#include <px4_platform_common/module.h>
-#include <uORB/uORB.h>
-#include <uORB/topics/manual_control_setpoint.h>
-#include <uORB/topics/actuator_controls.h>
-#include <px4_platform_common/px4_config.h>
-#include <drivers/drv_hrt.h>
-
-class ServoControl : public ModuleBase<ServoControl>
-{
-public:
-    int run() override;
-
-private:
-    float _pwm_norm = 0.0f;  // normalized PWM value [0.0 to 1.0]
-    bool _prev_button = false;  // for edge detection
-
-    void send_pwm_value(float value);
-};
+#include "servo_control.h"
 
 int ServoControl::run()
 {
@@ -24,7 +7,7 @@ int ServoControl::run()
     fds.fd = sub;
     fds.events = POLLIN;
 
-    PX4_INFO("Servo control started");
+    PX4_INFO("Servo Control Module started");
 
     while (!should_exit()) {
         int ret = px4_poll(&fds, 1, 100);
@@ -33,12 +16,10 @@ int ServoControl::run()
             manual_control_setpoint_s ctrl{};
             orb_copy(ORB_ID(manual_control_setpoint), sub, &ctrl);
 
-            // Button 1 pressed? (Change to button index as needed)
-            bool button = ctrl.buttons & (1 << 0);  // button 0
+            bool button = ctrl.buttons & (1 << 0);  // Button 0
 
-            if (button && !_prev_button) {
-                // rising edge detected
-                _pwm_norm += 0.05f;  // increment by 5%
+            if (button && !_prev_button) { // rising edge
+                _pwm_norm += 0.05f;
                 if (_pwm_norm > 1.0f) _pwm_norm = 1.0f;
 
                 PX4_INFO("PWM increased to %.2f", _pwm_norm);
@@ -56,6 +37,6 @@ void ServoControl::send_pwm_value(float value)
 {
     actuator_controls_s act{};
     act.timestamp = hrt_absolute_time();
-    act.control[5] = value; // AUX5 corresponds to actuator_controls_0[5]
+    act.control[5] = value; // Index 5 = AUX5
     orb_advert_t pub = orb_advertise(ORB_ID(actuator_controls_0), &act);
 }
